1c3de961a2c99b4286bcc31af7c1fc6d06eb4385
John ZuHone <jzuhone@gmail.com>
2018-06-20 22:46:30 -0400

    Import these at the top

--- acisfp_check/__init__.py
+++ acisfp_check/__init__.py
@@ -1,7 +1,11 @@
 __version__ = "2.3.0"
 
 from .acisfp_check import \
-    ACISFPCheck, calc_model
+    ACISFPCheck, calc_model, \
+    validation_limits, \
+    hist_limit, \
+    model_path, \
+    default_nopref_list
 
 from .acis_obs import \
     ObsidFindFilter
