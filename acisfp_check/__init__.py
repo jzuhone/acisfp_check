d1a13015597d3a4bc73056acd5442b684b5497eb
John ZuHone <jzuhone@gmail.com>
2020-05-16 15:12:06 -0400

    Forgot to add a hook for testing into __init__

--- acisfp_check/__init__.py
+++ acisfp_check/__init__.py
@@ -5,3 +5,11 @@ __version__ = ska_helpers.get_version(__package__)
 from .acisfp_check import \
     ACISFPCheck, main, \
     model_path
+
+
+def test(*args, **kwargs):
+    """
+    Run py.test unit tests.
+    """
+    import testr
+    return testr.test(*args, **kwargs)
