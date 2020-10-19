a583f83dbed67d67cee4ced33ce3dba934f05d25
John ZuHone <jzuhone@gmail.com>
2020-10-19 13:28:32 -0400

    Refactor tests to match new signature

--- acisfp_check/tests/test_acisfp_sql.py
+++ acisfp_check/tests/test_acisfp_sql.py
@@ -3,19 +3,21 @@ from acis_thermal_check.regression_testing import \
     RegressionTester, all_loads
 import pytest
 
-acisfp_rt = RegressionTester(ACISFPCheck, model_path, "acisfp_test_spec.json")
 
-# SQL state builder tests
-
-acisfp_rt.run_models(state_builder='sql')
+@pytest.fixture(autouse=True, scope='module')
+def fp_rt(test_root):
+    # SQL state builder tests
+    rt = RegressionTester(ACISFPCheck, model_path, "acisfp_test_spec.json",
+                          test_root=test_root, sub_dir='sql')
+    rt.run_models(state_builder='sql')
+    return rt
 
 # Prediction tests
 
-
 @pytest.mark.parametrize('load', all_loads)
-def test_prediction(answer_store, load):
+def test_prediction(fp_rt, answer_store, load):
     if not answer_store:
-        acisfp_rt.run_test("prediction", load)
+        fp_rt.run_test("prediction", load)
     else:
         pass
 
@@ -23,8 +25,8 @@ def test_prediction(answer_store, load):
 
 
 @pytest.mark.parametrize('load', all_loads)
-def test_validation(answer_store, load):
+def test_validation(fp_rt, answer_store, load):
     if not answer_store:
-        acisfp_rt.run_test("validation", load)
+        fp_rt.run_test("validation", load)
     else:
         pass
